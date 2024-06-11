a5fc6bd4ae4a61044ec99483a34ba5f42c9669f2
yashvoladoddi37 <89261520+yashvoladoddi37@users.noreply.github.com>
2024-06-11 14:38:33 +0530

    chat interface, multimodal interface begin

---
+++ chat.py
@@ -0,0 +1,89 @@
+import ollama
+import streamlit as st
+from openai import OpenAI
+from utilities.icon import page_icon
+
+st.set_page_config(
+    page_title="The Chat Window",
+    page_icon="💬",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+
+def extract_model_names(models_info: list) -> tuple:
+    """
+    Extracts the model names from the models information.
+
+    models_info: A dictionary containing the models' information.
+
+    Returns model names.
+    """
+
+    return tuple(model["name"] for model in models_info["models"])
+
+
+def main():
+    """
+    The main function that runs the application, built on Streamlit.
+    """
+
+    page_icon("💬")
+    st.subheader("Ollama Arena", divider="blue", anchor=False)
+
+    client = OpenAI(
+        base_url="http://localhost:11434/v1",
+        api_key="yashv_ollama",  # required, but unused
+    )
+
+    models_info = ollama.list()
+    available_models = extract_model_names(models_info)
+
+    if available_models:
+        selected_model = st.selectbox(
+            "Pick a model available locally on your system ↓", available_models
+        )
+
+    else:
+        st.warning("You have not pulled any model from Ollama yet!", icon="❌")
+        if st.button("Go to the settings page to download/delete a model"):
+            st.page_switch("settings.py")
+
+    message_container = st.container(height=500, border=True)
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        avatar = "🤖" if message["role"] == "assistant" else "🙋‍♂️"
+        with message_container.chat_message(message["role"], avatar=avatar):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Enter your prompt here"):
+        try:
+            st.session_state.messages.append(
+                {"role": "user", "content": prompt})
+
+            message_container.chat_message("user", avatar="🙋‍♂️").markdown(prompt)
+
+            with message_container.chat_message("assistant", avatar="🤖"):
+                with st.spinner("model working..."):
+                    stream = client.chat.completions.create(
+                        model=selected_model,
+                        messages=[
+                            {"role": m["role"], "content": m["content"]}
+                            for m in st.session_state.messages
+                        ],
+                        stream=True,
+                    )
+                # stream response
+                response = st.write_stream(stream)
+            st.session_state.messages.append(
+                {"role": "assistant", "content": response})
+
+        except Exception as e:
+            st.error(e, icon="⛔️")
+
+
+if __name__ == "__main__":
+    main()
